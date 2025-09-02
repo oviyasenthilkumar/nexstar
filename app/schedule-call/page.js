8b26a5943f629be4b55605435642e15dd33a4502
SUPRIYA M <167345798+Supriyamurugesan@users.noreply.github.com>
2025-09-02 10:57:47 +0530

    Update page.js

--- app/schedule-call/page.js
+++ app/schedule-call/page.js
@@ -3,13 +3,7 @@ export default function ScheduleCall() {
       <section className="min-h-screen flex flex-col items-center justify-center px-6 py-12">
         <h2 className="text-3xl font-bold mb-6">Schedule a Discovery Call</h2>
         <div className="w-full max-w-3xl">
-          <iframe
-            src="https://calendly.com/sudhikshaa06"
-            width="100%"
-            height="600"
-            frameBorder="0"
-            className="rounded-xl shadow"
-          ></iframe>
+          
         </div>
       </section>
     );
