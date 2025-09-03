c67335d74d364989c2c0da28e9861203a103d7cf
SUPRIYA M <167345798+Supriyamurugesan@users.noreply.github.com>
2025-09-03 13:24:34 +0530

    Update page.js

--- app/terms/page.js
+++ app/terms/page.js
@@ -3,7 +3,7 @@
 export default function TermsConditions() {
   return (
     <div className="bg-white min-h-screen w-full">
-      <div className="max-w-4xl mx-auto px-6 py-12 font-['League_Spartan'] text-gray-800 leading-relaxed">
+      <div className="max-w-4xl mx-auto px-6 py-12 font-['League_Spartan'] text-gray-800 leading-relaxed mt-15">
       {/* Header Section */}
       <section className="text-center mb-12">
         <h1 className="text-4xl font-bold mb-4">Terms & Conditions</h1>
